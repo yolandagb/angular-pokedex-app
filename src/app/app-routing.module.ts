7e39d425eca6fbba7be70aaf916a18f04706ed9e
Yolanda <yolandagarciabarranco@hotmail.com>
2025-04-11 13:19:48 +0200

    card neavigate to detail and main scss styles done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonDetailComponent } from './features/pokemon/pages/pokemon-detail/pokemon-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'pokemon', pathMatch: 'full' }, // Redirige a la página de Pokémon
+  { path: '', redirectTo: 'pokemon', pathMatch: 'full' }, 
   { path: 'pokemon', loadChildren: () => import('./features/pokemon/pokemon.module').then(m => m.PokemonModule) },
-  { path: '**', redirectTo: 'pokemon' }, // Redirige cualquier ruta no encontrada
+  { path: '**', redirectTo: 'pokemon' }, 
+  { path: 'detail/:id', component: PokemonDetailComponent },
 ];
 
 @NgModule({
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,17 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { CapitalizePipe } from '../../pipes/capitalize.pipe';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
   standalone: true,
-  imports: [MatCardModule,CommonModule],
+  imports: [MatCardModule,CommonModule,CapitalizePipe],
 })
 export class CardComponent {
   @Input() pokemonNames: any[] = []; 
-  ngOnChanges(): void {
-    console.log('Pokemon Names:', this.pokemonNames);
+  constructor(private router: Router) {}
+
+  navigateToDetail(id: number): void {
+    this.router.navigate(['/pokemon', id]); 
   }
 }
