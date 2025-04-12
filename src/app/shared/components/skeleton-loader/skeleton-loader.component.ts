19fb90f0a451cab2e238694e273289e9674e9aae
Yolanda <yolandagarciabarranco@hotmail.com>
2025-04-13 00:51:42 +0200

    skeleton

---
+++ src/app/shared/components/skeleton-loader/skeleton-loader.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-skeleton-loader',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skeleton-loader.component.html',
+  styleUrls: ['./skeleton-loader.component.scss']
+})
+export class SkeletonLoaderComponent {
+  @Input() count: number = 6;
+
+}
+
+
